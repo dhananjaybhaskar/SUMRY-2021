dba13f34f9a6e402d482a8b9afaace6b42636bfc
dhananjaybhaskar <dbhaskar92@gmail.com>
2022-05-16 15:20:56 -0400

    Initial implementation of diffusion curvature

--- plt_random_2D_quadric.m
+++ plt_random_2D_quadric.m
@@ -2,8 +2,10 @@ close all; clear all;
 addpath(genpath(pwd));
 rng('shuffle');
 
-n_trials = 10;
+n_trials = 6;
 quad_lim = 1e3;
+
+fig_cnt = 1;
 
 for i = 1:n_trials
 
@@ -11,9 +13,9 @@ for i = 1:n_trials
     %Q = RandOrthMat(2);
     %Lambda = diag(-quad_lim + quad_lim * rand(2,1));
     %A = Q * Lambda * Q'; 
-    A = quad_lim .* rand(2);
+    A = -quad_lim + quad_lim.*rand(2);
     
-    b = 10*rand(1,2);
+    b = -quad_lim + quad_lim.*rand(1,2);
     R = rand(1);
 
     % Find local optimum, evaluate hessian at optimum 
@@ -29,22 +31,33 @@ for i = 1:n_trials
     mesh_pts = [reshape(X, [numel(X), 1]), reshape(Y, [numel(Y), 1])];
     Z = cellfun(@(x) [x(1),x(2)]*(A*[x(1),x(2)]') + b*[x(1),x(2)]' + R, num2cell(mesh_pts,2));
 
-    % Sample 500 points around optimum
-    sample_pts = Xopt + randsphere(500, 2, 0.25);
+    % Sample 1000 points around optimum
+    sample_pts = Xopt + randsphere(1000, 2, 0.25);
     Z_pts = cellfun(@(x) [x(1),x(2)]*(A*[x(1),x(2)]') + b*[x(1),x(2)]' + R, num2cell(sample_pts,2));
 
     % Visualize
-    fig = figure(i);
+    fig = figure(fig_cnt);
     surf(X, Y, reshape(Z, size(X)),reshape(Z, size(X)))
+    shading interp
+    camlight
+    axis off
+    grid off
+    %hold off
+    set(gca, 'XColor', 'none', 'YColor', 'none', 'ZColor', 'none')
+    print(fig, strcat('quadric_', string(i)), '-r800', '-dpng');
+    fig_cnt = fig_cnt + 1;
+
+    fig = figure(fig_cnt);
+    scatter3(sample_pts(:,1), sample_pts(:,2), Z_pts, 18, Z_pts, "filled")
     hold on
-    scatter3(sample_pts(:,1), sample_pts(:,2), Z_pts, 5, Z_pts, "red", "filled")
-    scatter3(Xopt(1), Xopt(2), Zopt, 25, "white", "filled", "o", "MarkerEdgeColor", "k")
+    scatter3(Xopt(1), Xopt(2), Zopt, 20, "red", "filled")
     shading interp
     camlight
     axis off
     grid off
     hold off
     set(gca, 'XColor', 'none', 'YColor', 'none', 'ZColor', 'none')
-    print(fig, strcat('quadric_', string(i)), '-r800', '-dpng');
+    print(fig, strcat('quadric_pts_', string(i)), '-r800', '-dpng');
+    fig_cnt = fig_cnt + 1;
 
 end
